90b263b11e94c0a71ae847018c4da14fcd41a94b
edcasill <acasillasval@gmail.com>
2026-06-01 19:54:18 -0600

    started code

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ import os
 import numpy as np
 
 
+def read_file(file):
+    """_summary_
+
+    Args:
+        file (_type_): _description_
+    """
+    with open(file, 'r') as f:
+        lines = f.readlines()
+
+
 def main():
     option = int(input('Selecciona el numero de archivo a analizar: '))
     print(option)
--- petri_nets_types.py
+++ petri_nets_types.py
@@ -2,5 +2,31 @@ import numpy as np
 
 
 class Petri_Nets:
-    def __init__(self):
-        pass
+    def __init__(self, pre, post):
+        self.pre = np.array(pre)
+        self.post = np.array(post)
+        self.C = self.post - self.pre
+
+    def is_state_machine(self):
+        """
+        Each transition just have 1 input place and 1 output place.
+        The summatory of each transition on C must be 0.
+        """
+        c_transpose = self.C.T
+        for row in c_transpose:
+            row_counter = np.sum(row)
+            if row_counter != 0:
+                print("This PN is not a SM, checking if it is a GM or FC")
+                break
+        print("This PN is a SM, each transition just have 1 input place and 1 output place.")
+        print("Also, for definition, it is a FC")
+
+    def is_marked_graph(self):
+        """
+        Each place just have 1 input transition and 1 output transition.
+        The summatory of each place on C must be 0.
+        """
+
+    def is_free_choice(self):
+        """_summary_
+        """
