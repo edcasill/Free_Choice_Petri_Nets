5274cbf9c0d5f9b8f1e4e6473fe68c4203818315
edcasill <acasillasval@gmail.com>
2026-06-28 20:21:40 -0600

    added martinez-silva algorithm

--- st_components.py
+++ st_components.py
@@ -13,8 +13,51 @@ class components:
         Calculate S-invariants and P invariants
         """
         C = self.post - self.pre
-        if use_transpose == True:
-            C = self.C.T
+        if use_transpose:
+            C = C.T
+
+        n_rows, m_cols = C.shape  # size the matrix
+        D = np.eye(n_rows, dtype=int)  # identity matrix
+        A = np.hstack((D,C)).astype(int)  # augmented matrix [D|C]
+
+        for i in range(m_cols):
+            col_index = n_rows + i
+            # filtering
+            A_zero = A[A[:, col_index] == 0]  # rows that are null on the column
+            A_pos = A[A[:, col_index] > 0]  # positive rows
+            A_neg = A[A[:, col_index] < 0]  # negative rows
+
+            new_rows = []
+
+            # lineal combinations
+            # anulate column using positive and negative rows
+            for row_p in A_pos:
+                for row_n in A_neg:
+                    pos_val = row_p[col_index]
+                    neg_val = abs(row_n[col_index])
+
+                    # MCD
+                    gcd_val = np.gcd(pos_val, neg_val)
+                    mult_p = neg_val // gcd_val
+                    mult_n = pos_val // gcd_val
+
+                    # new null row
+                    new_row = (mult_p * row_p) + (mult_n * row_n)
+                    new_rows.append(new_row)
+
+                    # update matrix
+                    # o rows + new rows
+                    if new_rows:
+                        A = np.vstack((A_zero, np.array(new_rows)))
+                    elif len(A_zero) > 0:
+                        A = A_zero
+                    else:
+                        break
+
+        invariants = A[:, n_rows]
+        invariants = invariants[~np.all(invariants == 0, axis=1)]
+
+        return invariants
 
     def s_components_filter(self, S_invariants):
         """
